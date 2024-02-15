c1ef77c305f37d34481cfb71c0084afdd424dcb7
clds <chintan.tops@gmail.com>
2024-02-15 12:26:56 +0530

    fie-exception

---
+++ File/demo.py
@@ -0,0 +1,37 @@
+# f = open("myfile.txt",'w')
+# f.write("Hello python")
+# f.truncate(4)
+# f.close()
+
+# f = open("myfile.txt",'r')
+# text = f.read()
+# print(text)
+
+# f = open("myfile.txt",'a')
+# f.write("Hello python")
+# f.close()
+
+# with open("myfile.txt") as f:
+#     f.seek(5)
+#     print(f.tell())
+#     text=f.read()
+#     print(text)
+
+# with open("myfile.txt") as f :
+#     text = f.readline()
+#     print(text)
+
+import os
+
+# os.makedirs("NEWFOLDER")
+# os.rmdir("NEWFOLDER")
+
+
+
+   
+
+
+
+    
+   
+
