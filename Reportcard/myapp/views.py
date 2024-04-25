b1cf08c939bd5d186f5ac579bdfd0067c6ec3280
chintan <chintan.tops@gmail.com>
2024-04-25 15:56:26 +0530

    all

--- Reportcard/myapp/views.py
+++ Reportcard/myapp/views.py
@@ -3,12 +3,22 @@ from .models import *
 from django.core.paginator import Paginator
 from django.db.models import Sum
 # Create your views here.
+
+
+global search_data
 def index(request):
+    
+    
+   
     queryset = Student.objects.all()
 
-    paginator = Paginator(queryset, 10)  
-
-    page_number = request.GET.get("page",1)
+    if request.GET.get("search"):
+        search_data = request.GET.get("search")
+        queryset = Student.objects.filter(name__icontains=search_data)
+    
+   
+    paginator = Paginator(queryset, 2)  
+    page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
 
 
